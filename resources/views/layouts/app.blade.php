38a7ca53384b4c068899e583e8117b37aedcdc0a
StefanWL <stefan_whittaker-lee@alumni.brown.edu>
2022-04-02 11:48:06 -0400

    attach bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,23 +5,27 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Sports Tinder</title>
-
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body class="bg-gray-200">
+    <body>
+        <nav class="navbar navbar-expand-xs navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    @auth
+                        <a class="nav-item nav-link" href="{{ route('conversations') }}" class="p-3">Conversations</a>
+                        <a class="nav-item nav-link" href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
+                        <a class="nav-item nav-link" href="{{ route('profile') }}" class="p-3">Profile</a>
+                    @endauth
+                </div>
+            </div>
+        </nav>
         <nav class="p-6 bg-white flex justify-between mb-6">
             <ul class="flex items-center">
-                <li>
-                    <a href="{{ route('conversations') }}" class="p-3">Conversations</a>
-                </li>
-                <li>
-                    <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
-                </li>
-                @auth
-                <li>
-                    <a href="{{ route('profile') }}" class="p-3">Profile</a>
-                </li>
-                @endauth
+                
             </ul>
 
             <ul class="flex items-center">
